4adf2dc717debbb3fb786963a4eede6bf01417e5
hoantat21 <36986868+hoantat21@users.noreply.github.com>
2023-05-06 21:20:52 +0700

    update code

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -28,7 +28,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get("/filteredimage", async (req, res) => {
+  app.get("/filteredimage", async (req: Request, res: Response) => {
     const {image_url} = req.query;
     if (!image_url) {
       res.status(400).send('bad request');
@@ -38,6 +38,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     res.on("finish", () => deleteLocalFiles([filteredpath]));
 
   })
+  
   //! END @TODO1
   
   // Root Endpoint
